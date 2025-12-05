9a84593acd06ea6171308741cbce6b3c9a20f5ad
kiran-kharsel <kirankharsel@gmail.com>
2025-12-05 07:06:36 +0530

    feat: implement modal closing functionality

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const openModalBtn = document.querySelector('.open-modal');
-const myDialog = document.querySelector('#myDialog')
+const myDialog = document.querySelector('#myDialog');
+const closeModalBtn = document.querySelector('.close-modal')
 
 openModalBtn.addEventListener('click', function(){
     myDialog.showModal()
 })
+
+closeModalBtn.addEventListener('click', function(){
+    console.log('workoing');
+    myDialog.close()
+})
+
+console.log(myDialog)
